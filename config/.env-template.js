bb64ca09168da11d8a051eca3777bbc912847f64
Daniel Kim <danieljoonkimm@gmail.com>
2019-03-11 10:31:33 -0700

    [add] adding files for rest-server

---
+++ config/.env-template.js
@@ -0,0 +1,33 @@
+const envBuild = {
+  'rest-server': [
+    'DEBUG=TRUE',
+    'NODE_ENV=test',
+    'PORT=3000',
+    'LOCAL_USER=root',
+    'LOCAL_HOST=localhost',
+    'AWS_USER=',
+    'AWS_HOST=',
+    'AWS_PASSWORD=',
+    'AWS_PORT=',
+  ],
+  'smtp-server': [
+    'DEBUG=TRUE',
+    'NODE_ENV=test',
+    'PORT=8080',
+    'PASSWORD=',
+    'EMAIL=',
+    'SECRET_KEY=',
+    'LOCAL_USER=root',
+    'LOCAL_HOST=localhost',
+    'AWS_USER=',
+    'AWS_HOST=',
+    'AWS_PASSWORD=',
+    'AWS_PORT=',
+  ],
+  'client': [
+    'NODE_ENV=development',
+    'REACT_APP_API_KEY=',
+  ]
+};
+
+module.exports = envBuild;
