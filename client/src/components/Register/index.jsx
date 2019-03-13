174d06c6d050be6c5f818496459f6617d2c07ae3
Daniel Kim <danieljoonkimm@gmail.com>
2019-03-13 11:00:59 -0700

    [add] adding register component

--- client/src/components/Register/index.jsx
+++ client/src/components/Register/index.jsx
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Register extends Component {
   constructor() {
     super();
-    
-    this.state = {
 
-    }
+    this.state = {};
   }
 
   render() {
-    return(
+    return (
       <div className="register_container">
         <h1 className="register_header">Sign Up</h1>
+        <div className="register_body">
+          <h2>It' free and anyone can join</h2>
+            <ul className="register_form">
+              <li>First Name: <input></input></li>
+              <li>Last Name: <input></input></li>
+              <li>Your Email: <input></input></li>
+              <li>New Password: <input></input></li>
+              <li>Confirm Password: <input></input></li>
+            </ul>
+        </div>
       </div>
-    )
+    );
   }
-};
+}
 
 export default Register;
