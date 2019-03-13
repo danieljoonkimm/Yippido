0f0d29756d02fe347d8359778e61bdeb48a6b0f1
Daniel Kim <danieljoonkimm@gmail.com>
2019-03-13 00:19:02 -0700

    [edit] fixing login

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,30 +1,29 @@
-import React, { Component } from 'react';
-import Login from './Login/index';
-import { Link, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import Login from "./Login/index";
+import { Link, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   constructor() {
     super();
 
-    this.state = {
-
-    }
+    this.state = {};
   }
 
   render() {
-    return(
+    return (
       <div>
         <ul>
-          <li><Link to="/login">Login</Link></li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
         </ul>
 
         <Switch>
           <Route exact path="/login" component={Login} />
         </Switch>
       </div>
-
-    )
+    );
   }
-};
+}
 
 export default App;
--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Login extends Component {
   constructor() {
     super();
 
     this.state = {
-      email: '',
-      password: ''
-    }
+      email: "",
+      password: ""
+    };
   }
 
   validateForm() {
-    if(this.state.email.length < 1 || this.state.password.length <1) {
-      alert('PLEASE ENTER VALID EMAIL OR PASSWORD')
+    if (this.state.email.length < 1 || this.state.password.length < 1) {
+      alert("PLEASE ENTER VALID EMAIL OR PASSWORD");
     }
   }
 
   handleChange(event) {
-    console.log(event.target.value)
+    console.log(event.target.value);
     this.setState({
-      [event.target.name] : event.target.value
+      [event.target.name]: event.target.value
     });
   }
 
@@ -28,18 +28,33 @@ class Login extends Component {
   }
 
   render() {
-    return(
-      <div>
+    return (
+      <div class="login_container">
         <form onSubmit={this.handleSubmit.bind(this)}>
           <label>
-            Email: <input type="text" name="email" onChange={this.handleChange.bind(this)}/><br/>
-            Password: <input type="text" name="password" onChange={this.handleChange.bind(this)}/>
+            Email:{" "}
+            <input
+              type="text"
+              name="email"
+              onChange={this.handleChange.bind(this)}
+            />
+            <br />
+            Password:{" "}
+            <input
+              type="text"
+              name="password"
+              onChange={this.handleChange.bind(this)}
+            />
           </label>
-          <input type="submit" value="Submit" onClick={this.validateForm.bind(this)}/>
+          <input
+            type="submit"
+            value="Submit"
+            onClick={this.validateForm.bind(this)}
+          />
         </form>
       </div>
-    )
+    );
   }
-};
+}
 
 export default Login;
