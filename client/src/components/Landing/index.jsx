6d48e21367a999b405d0e7b685f8b1e69ce344d9
Daniel Kim <danieljoonkimm@gmail.com>
2019-03-14 22:34:03 -0700

    [edit] adding Landing page and need to re-route with Router to Login if clicking login

---
+++ client/src/components/Landing/index.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    }
+  }
+  
+  render() {
+    return(
+      <div>
+        LANDING PAGE
+      </div>
+    )
+  }
+};
+
+export default Landing;
