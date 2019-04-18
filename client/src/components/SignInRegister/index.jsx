01007c5e3a2ad2f4ad2173e29ee5b12eecb74063
Daniel Kim <danieljoonkimm@gmail.com>
2019-04-18 00:37:13 -0700

    [edit] fixing

--- client/src/components/SignInRegister/index.jsx
+++ client/src/components/SignInRegister/index.jsx
@@ -60,7 +60,7 @@ class SignInRegister extends Component {
           </div>
           <div className="row">
           <div className="col-xs-6">New Members 15% OFF</div>
-          <div className="col-xs-6">REGISTER NOW</div>
+          <div className="col-xs-6 register_now">REGISTER NOW</div>
           </div>
         </div>
         <div className="signIn_becomeVendor">
@@ -76,7 +76,7 @@ class SignInRegister extends Component {
           <div className="col-xs-6">
           3 MONTH FREE BANNER
           </div>
-          <div className="col-xs-6">REGISTER NOW</div>
+          <div className="col-xs-6 register_now">REGISTER NOW</div>
           </div>
         </div>
       </div>
