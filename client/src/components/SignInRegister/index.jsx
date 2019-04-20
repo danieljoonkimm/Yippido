ff7a4b9f13be7ad802f78630f00017804f2d94a0
Daniel Kim <danieljoonkimm@gmail.com>
2019-04-20 16:06:29 -0700

    [edit] finishing first register

--- client/src/components/SignInRegister/index.jsx
+++ client/src/components/SignInRegister/index.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import "./signInRegister.scss";
 import Yippido from "../../../public/images/Yippido.png";
 import magnifying_glass from "../../../public/images/magnifying_glass.jpg";
+import topdeals from "../../../public/images/topdeals.jpeg";
 
 class SignInRegister extends Component {
   constructor() {
@@ -12,69 +13,62 @@ class SignInRegister extends Component {
   }
   render() {
     return (
-      <div className="signInRegister_container">
-        <div className="signInRegister_header">
-          <img src={Yippido} alt="No Image" />
-          <div className="col-xs-12 signInRegister_searchBar">
-            <button>All</button> |{" "}
-            <input
-              type="text"
-              placeholder="type vendor name, item description, or style number"
-            />{" "}
-            | <img src={magnifying_glass} />
-          </div>
-          <div className="row signInRegister_text">
-            <button className="col-xs-6" id="register">
-              Register
-            </button>
-            <button className="col-xs-6" id="signIn">
-              Sign In
-            </button>
+      <div className="registration">
+        <div className="container">
+          <div className="header">
+            <h1>Registration</h1>
+            <div className="languageBtn_container">
+              <button>English</button>
+              <button>Spanish</button>
+            </div>
+            <p>
+              Join the premier online shopping mall for wholesale fashion and
+              lifestyle brands
+            </p>
+            <p>
+              A community of 900+ wholesale vendors & nearly half a million
+              buyers.
+            </p>
           </div>
         </div>
-        <div className="row signInRegister_registration">
-          <div className="row signInRegister_languages">
-            <h1 className="col-xs-6">registration</h1>
-            <div className="signIn_languages">
-              {" "}
-              <div className="col-xs-6">
-                <button className="english">English</button>
-                <button className="spanish">Spanish</button>
-              </div>
+
+        <div className="row become_container">
+          <div className="row become">
+            <h2>Become a BUYER</h2>
+            <div className="become_text">
+              <p>Free Membership</p>
+              <p>Shop thousands of new styles uploaded daily</p>
+              <p>Make smart purchases with our data & trend analytics</p>
             </div>
           </div>
-          <div className="signIn_text">
-            Join the premier online shopping mall for wholesale fashion and
-            lifestyle brands
-            <br />A community of 900+ vendors and nearly half a million buyers
+
+          <div className="row become_register">
+            <div className="col-xs-6 become_img">
+              <img src={topdeals} alt="" />
+            </div>
+            <div className="col-xs-6 become_button">
+              <button>REGISTER NOW</button>
+            </div>
           </div>
         </div>
-        <div className="signIn_becomeBuyer">
-          <h2>Become a BUYER</h2>
-          <div className="signIn_buyerText">
-            Free Membership
-            <br />
-            Shop thousands of new styles uploaded daily
-            <br />
-            Make smart purchases with our data and trend analytics
+
+        <div className="row become_container" id="buyer">
+          <div className="row become">
+            <h2>Become a BUYER</h2>
+            <div className="become_text">
+              <p>Discover new sales opportunities</p>
+              <p>Market your brand to thousands of qualified buyers</p>
+              <p>Get personalized support with a dedicated account executive</p>
+            </div>
           </div>
-          <div className="row promotions">
-            <div className="col-xs-6">New Members 15% OFF</div>
-            <div className="col-xs-6 register_now"><button>REGISTER NOW</button></div>
-          </div>
-        </div>
-        <div className="signIn_becomeVendor">
-          <h2>Become a VENDOR</h2>
-          <div className="signIn_vendorText">
-            Discover new sales opportunities
-            <br />
-            Market your brand to thousands of qualified buyers
-            <br />
-            Get personalized support with a dedicated account executive
-          </div>
-          <div className="row promotions">
-            <div className="col-xs-6">3 MONTH FREE BANNER</div>
-            <div className="col-xs-6 register_now"><button>REGISTER NOW</button></div>
+
+          <div className="row become_register">
+            <div className="col-xs-6 become_img">
+              <img src={topdeals} alt="" />
+            </div>
+            <div className="col-xs-6 become_button">
+              <button>REGISTER NOW</button>
+            </div>
           </div>
         </div>
       </div>
