e07a17f130ef1e536f48ebdf3ebe8058042168d8
Daniel Kim <danieljoonkimm@gmail.com>
2019-04-23 21:14:23 -0700

    [edit] fixing

--- client/src/components/landing/lookbook/index.jsx
+++ client/src/components/landing/lookbook/index.jsx
@@ -17,20 +17,7 @@ class LookBook extends Component {
           <div className="col-xs-4 col-sm-4 col-md-4">
             <div className="row">
               <img className="img-responsive" alt="" src={lookbook} />
-              <p>SAMPLE</p>
-              <p>SAMPLE</p>
-            </div>
-          </div>
-
-          <div className="col-xs-4 col-sm-4 col-md-4">
-            <div className="row">
-              <div className="col-xs-6 col-sm-6 col-md-6">
-                <img className="img-responsive" alt="" src={lookbook1} />
-                <p>SAMPLE</p>
-                <p>SAMPLE</p>
-              </div>
-              <div className="col-xs-6 col-sm-6 col-md-6">
-                <img className="img-responsive" alt="" src={lookbook1} />
+              <div>
                 <p>SAMPLE</p>
                 <p>SAMPLE</p>
               </div>
@@ -41,14 +28,30 @@ class LookBook extends Component {
             <div className="row">
               <div className="col-xs-6 col-sm-6 col-md-6">
                 <img className="img-responsive" alt="" src={lookbook1} />
-                <p>SAMPLE</p>
-                <p>SAMPLE</p>
               </div>
               <div className="col-xs-6 col-sm-6 col-md-6">
                 <img className="img-responsive" alt="" src={lookbook1} />
+                <div />
+              </div>
+              <div>
                 <p>SAMPLE</p>
                 <p>SAMPLE</p>
               </div>
+            </div>
+          </div>
+
+          <div className="col-xs-4 col-sm-4 col-md-4">
+            <div className="row">
+              <div className="col-xs-6 col-sm-6 col-md-6">
+                <img className="img-responsive" alt="" src={lookbook1} />
+              </div>
+              <div className="col-xs-6 col-sm-6 col-md-6">
+                <img className="img-responsive" alt="" src={lookbook1} />
+              </div>
+              <div>
+                  <p>SAMPLE</p>
+                  <p>SAMPLE</p>
+                </div>
             </div>
           </div>
         </div>
