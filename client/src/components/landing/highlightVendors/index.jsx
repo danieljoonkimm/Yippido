724374ce07aa5b98dea95b6c792ff1a3583a9eb2
Daniel Kim <danieljoonkimm@gmail.com>
2019-04-24 00:17:39 -0700

    [edit] fixing borders

--- client/src/components/landing/highlightVendors/index.jsx
+++ client/src/components/landing/highlightVendors/index.jsx
@@ -15,56 +15,56 @@ class HighlightVendors extends Component {
         <div className="row">
           <div className="col-xs-3 col-sm-3 col-md-3">
             <div className="col-xs-12 col-sm-12 col-md-12">
-              <img className="img-responsive" alt="" src={dummyImg} />
+              <img className="img-responsive highlightVendors_bigImage" alt="" src={dummyImg} />
             </div>
           </div>
 
           <div className="col-xs-3 col-sm-3 col-md-3">
             <div className="row">
               <div className="col-xs-6 col-sm-6 col-md-6">
-                <img className="img-responsive" alt="" src={dummyImg} />
+                <img className="img-responsive" id="highlightVendors_topLeft" alt="" src={dummyImg} />
               </div>
 
               <div className="col-xs-6 col-sm-6 col-md-6">
-                <img className="img-responsive" alt="" src={dummyImg} />
+                <img className="img-responsive" id="highlightVendors_topRight" alt="" src={dummyImg} />
               </div>
             </div>
 
             <div className="row">
               <div className="col-xs-6 col-sm-6 col-md-6">
-                <img className="img-responsive" alt="" src={dummyImg} />
+                <img className="img-responsive" id="highlightVendors_bottomLeft" alt="" src={dummyImg} />
               </div>
 
               <div className="col-xs-6 col-sm-6 col-md-6">
-                <img className="img-responsive" alt="" src={dummyImg} />
+                <img className="img-responsive" id="highlightVendors_bottomRight" alt="" src={dummyImg} />
               </div>
             </div>
           </div>
 
           <div className="col-xs-3 col-sm-3 col-md-3">
             <div className="col-xs-12 col-sm-12 col-md-12">
-              <img className="img-responsive" alt="" src={dummyImg} />
+              <img className="img-responsive highlightVendors_bigImage" alt="" src={dummyImg} />
             </div>
           </div>
 
           <div className="col-xs-3 col-sm-3 col-md-3">
             <div className="row">
               <div className="col-xs-6 col-sm-6 col-md-6">
-                <img className="img-responsive" alt="" src={dummyImg} />
+                <img className="img-responsive" id="highlightVendors_topLeft" alt="" src={dummyImg} />
               </div>
 
               <div className="col-xs-6 col-sm-6 col-md-6">
-                <img className="img-responsive" alt="" src={dummyImg} />
+                <img className="img-responsive" id="highlightVendors_topRight" alt="" src={dummyImg} />
               </div>
             </div>
 
             <div className="row">
               <div className="col-xs-6 col-sm-6 col-md-6">
-                <img className="img-responsive" alt="" src={dummyImg} />
+                <img className="img-responsive" id="highlightVendors_bottomLeft" alt="" src={dummyImg} />
               </div>
 
               <div className="col-xs-6 col-sm-6 col-md-6">
-                <img className="img-responsive" alt="" src={dummyImg} />
+                <img className="img-responsive" id="highlightVendors_bottomRight" alt="" src={dummyImg} />
               </div>
             </div>
           </div>
