ed3c832de0cbfe12aa9bb40079fa3a6536c245f9
Daniel Kim <danieljoonkimm@gmail.com>
2019-04-24 01:44:48 -0700

    [edit] need to go fix lookbook later

--- client/src/components/landing/lookbook/index.jsx
+++ client/src/components/landing/lookbook/index.jsx
@@ -14,35 +14,35 @@ class LookBook extends Component {
         <div className="row">
           <h1>FROM THE LOOK BOOK</h1>
         </div>
-        <div className="row">
-          <div className="col-xs-4 col-sm-4 col-md-4">
+        <div className="row" id="lookBook_row">
+          <div className="col-xs-4 col-sm-4 col-md-4" id="lookBookMain_mainImage">
             <img className="img-responsive" alt="" src={dummyImg} />
             <p>SAMPLE</p>
             <p>SAMPLE</p>
           </div>
 
-          <div className="col-xs-4 col-sm-4 col-md-4">
+          <div className="col-xs-4 col-sm-4 col-md-4 lookBookMain_subParent">
             <div className="row">
-              <div className="col-xs-6 col-sm-6 col-md-6">
+              <div className="col-xs-6 col-sm-6 col-md-6 lookBookMain_subImage">
                 <img className="img-responsive" alt="" src={dummyImg} />
                 <p>SAMPLE</p>
                 <p>SAMPLE</p>
               </div>
-              <div className="col-xs-6 col-sm-6 col-md-6">
+              <div className="col-xs-6 col-sm-6 col-md-6 lookBookMain_subImage">
                 <img className="img-responsive" alt="" src={dummyImg} />
                 <div />
               </div>
             </div>
           </div>
 
-          <div className="col-xs-4 col-sm-4 col-md-4">
+          <div className="col-xs-4 col-sm-4 col-md-4 lookBookMain_subParent">
             <div className="row">
-              <div className="col-xs-6 col-sm-6 col-md-6">
+              <div className="col-xs-6 col-sm-6 col-md-6 lookBookMain_subImage">
                 <img className="img-responsive" alt="" src={dummyImg} />
                 <p>SAMPLE</p>
                 <p>SAMPLE</p>
               </div>
-              <div className="col-xs-6 col-sm-6 col-md-6">
+              <div className="col-xs-6 col-sm-6 col-md-6 lookBookMain_subImage">
                 <img className="img-responsive" alt="" src={dummyImg} />
               </div>
             </div>
