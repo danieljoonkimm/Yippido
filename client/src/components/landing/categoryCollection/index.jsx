336bc585434965c1c8a4e0b51e89ca2411062e89
Daniel Kim <danieljoonkimm@gmail.com>
2019-04-25 12:03:28 -0700

    [edit] trying to render category list

--- client/src/components/landing/categoryCollection/index.jsx
+++ client/src/components/landing/categoryCollection/index.jsx
@@ -7,27 +7,51 @@ class CategoryCollection extends Component {
     super();
 
     this.state = {
-      data: [
-        {"name": "women"},
-        {"name": "men"},
-        {"name": "kid"},
-        {"name": "shoes"},
-        {"name": "accessories"},
-        {"name": "handbags"},
-        {"name": "others"},
-      ]
+      categories: [
+        {
+          women: {
+            women1:
+              "https://images.unsplash.com/photo-1553531384-ab9ba1a8b1e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=60&q=80",
+            women2:
+              "https://images.unsplash.com/photo-1553531384-ab9ba1a8b1e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=60&q=80",
+            women3:
+              "https://images.unsplash.com/photo-1553531384-ab9ba1a8b1e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=60&q=80"
+          }
+        },
+        {
+          shoes: {
+            booties: "shoes",
+            boots: "test",
+            dress_shoes: "test1"
+          }
+        }
+      ],
+      eachCategory: [],
     };
   }
+
+  componentDidMount() {
+    {
+      this.state.categories.map(categories => {
+        let cat = categories;
+        for (var key in cat) {
+          this.state.eachCategory.push(key);
+        }
+      });
+    }
+  }
+
   render() {
+    console.log(this.state.eachCategory)
     return (
       <div className="container" id="categoryCollection_container">
         <h1>CATEGORY COLLECTION</h1>
         <div className="row">
           <div className="col-xs-2 col-sm-2 col-sm-2">
             <div className="col-xs-12 col-sm-12 col-md-12">
-              {this.state.data.map(categories => {
-                console.log(categories);
-              })}
+            {this.state.eachCategory.map((list) => {
+              return <li>{list}</li>
+            })}
             </div>
           </div>
 
