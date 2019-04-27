82112d81d944849a228aef5871d0266a2c79d2b8
Daniel Kim <danieljoonkimm@gmail.com>
2019-04-26 20:14:04 -0700

    [edit] adding newVendors component

--- client/src/components/landing/index.jsx
+++ client/src/components/landing/index.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import Vendors from './vendors/index.jsx';
-import FeaturedItems from './featuredItems/index.jsx';
-import HighlightVendors from './highlightVendors/index.jsx';
-import LookBook from './lookbook/index.jsx';
-import CategoryCollection from './categoryCollection/index.jsx';
+import Vendors from "./vendors/index.jsx";
+import FeaturedItems from "./featuredItems/index.jsx";
+import HighlightVendors from "./highlightVendors/index.jsx";
+import LookBook from "./lookbook/index.jsx";
+import CategoryCollection from "./categoryCollection/index.jsx";
+import NewVendors from "./newVendors/index.jsx";
 import "./landing.scss";
 
 class Landing extends Component {
@@ -15,11 +16,12 @@ class Landing extends Component {
   render() {
     return (
       <div>
-        <Vendors/>
-        <FeaturedItems/>
-        <HighlightVendors/>
-        <LookBook/>
-        <CategoryCollection/>
+        <Vendors />
+        <FeaturedItems />
+        <HighlightVendors />
+        <LookBook />
+        <CategoryCollection />
+        <NewVendors/>
       </div>
     );
   }
---
+++ client/src/components/landing/newVendors/index.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import dummyImg from "../../../../public/images/dummyImg.jpg";
+import "./newVendors.scss";
+
+class NewVendors extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="container" id="highlightVendors_container">
+        <h1>NEW VENDORS</h1>
+        <div className="row" id="highlightVendors_row">
+          <div className="col-xs-3 col-sm-3 col-md-3">
+            <div className="col-xs-12 col-sm-12 col-md-12">
+              <img className="img-responsive highlightVendors_bigImage" alt="" src={dummyImg} />
+            </div>
+          </div>
+
+          <div className="col-xs-3 col-sm-3 col-md-3">
+            <div className="row">
+              <div className="col-xs-6 col-sm-6 col-md-6">
+                <img className="img-responsive" id="highlightVendors_topLeft" alt="" src={dummyImg} />
+              </div>
+
+              <div className="col-xs-6 col-sm-6 col-md-6">
+                <img className="img-responsive" id="highlightVendors_topRight" alt="" src={dummyImg} />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-xs-6 col-sm-6 col-md-6">
+                <img className="img-responsive" id="highlightVendors_bottomLeft" alt="" src={dummyImg} />
+              </div>
+
+              <div className="col-xs-6 col-sm-6 col-md-6">
+                <img className="img-responsive" id="highlightVendors_bottomRight" alt="" src={dummyImg} />
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xs-3 col-sm-3 col-md-3">
+            <div className="col-xs-12 col-sm-12 col-md-12">
+              <img className="img-responsive highlightVendors_bigImage" alt="" src={dummyImg} />
+            </div>
+          </div>
+
+          <div className="col-xs-3 col-sm-3 col-md-3">
+            <div className="row">
+              <div className="col-xs-6 col-sm-6 col-md-6">
+                <img className="img-responsive" id="highlightVendors_topLeft" alt="" src={dummyImg} />
+              </div>
+
+              <div className="col-xs-6 col-sm-6 col-md-6">
+                <img className="img-responsive" id="highlightVendors_topRight" alt="" src={dummyImg} />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-xs-6 col-sm-6 col-md-6">
+                <img className="img-responsive" id="highlightVendors_bottomLeft" alt="" src={dummyImg} />
+              </div>
+
+              <div className="col-xs-6 col-sm-6 col-md-6">
+                <img className="img-responsive" id="highlightVendors_bottomRight" alt="" src={dummyImg} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewVendors;
