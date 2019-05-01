dd81a5574ec8dd18693805d51f069814470f75c0
Daniel Kim <danieljoonkimm@gmail.com>
2019-04-30 22:22:24 -0700

    [edit] fixing shopping cart

--- client/src/components/ShoppingCart/VendorShoppingCartSavedItems/index.jsx
+++ client/src/components/ShoppingCart/VendorShoppingCartSavedItems/index.jsx
@@ -51,141 +51,76 @@ class VendorShoppingCartSavedItems extends Component {
   render() {
     return (
       <div className="container" id="vendorShoppingCartSavedItems_container">
-        <div className="row">
-          <div className="col-sm-12 col-md-10 col-md-offset-1">
-            <table className="table table-hover">
-              <thead>
-                <tr>
-                  <th>Pic</th>
-                  <th>Style No.</th>
-                  <th className="text-center">Color</th>
-                  <th className="text-center">Size</th>
-                  <th>Pack</th>
-                  <th>Total Qty</th>
-                  <th>Unit Price</th>
-                  <th>Amount</th>
-                  <th>Sel</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td className="col-xs-3 col-sm-3 col-md-3">
-                    <div className="media">
-                      <a className="thumbnail pull-left" href="#">
-                        {" "}
-                        <img
-                          style={{ width: "72px", height: "72px" }}
-                          className="media-object"
-                          src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png"
-                        />
-                      </a>
-                      <div className="media-body">
-                        <h4 className="media-heading">
-                          <a href="#">Product name</a>
-                        </h4>
-                        <h5 className="media-heading">
-                          {" "}
-                          by <a href="#">Brand name</a>
-                        </h5>
-                        <span>Status: </span>
-                        <span className="text-success">
-                          <strong>In Stock</strong>
-                        </span>
-                      </div>
-                    </div>
-                  </td>
-
-                  <td className="col-sm-1 col-md-1">
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="exampleInputEmail1"
-                      value="3"
+        <table id="cart" className="table table-hover table-condensed">
+          <thead>
+            <tr>
+              <th style={{width:"50%"}}>Product</th>
+              <th style={{width:"10%"}}>Price</th>
+              <th style={{width:"8%"}}>Quantity</th>
+              <th style={{width:"22%"}} className="text-center">
+                Subtotal
+              </th>
+              <th style={{width:"10%"}} />
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td data-th="Product">
+                <div className="row">
+                  <div className="col-sm-2 hidden-xs">
+                    <img
+                      src="http://placehold.it/100x100"
+                      alt="..."
+                      className="img-responsive"
                     />
-                  </td>
-                  <td className="col-sm-1 col-md-1 text-center">
-                    {<strong>RED</strong>}
-                  </td>
-                  <td className="col-sm-1 col-md-1 text-center">
-                    <strong>S-M-L</strong>
-                  </td>
-                  <td className="col-sm-1 col-md-1 text-center">
-                    <strong>3</strong>
-                  </td>
-                  <td className="col-sm-1 col-md-1 text-center">
-                    <strong>10</strong>
-                  </td>
-                  <td className="col-sm-1 col-md-1 text-center">
-                    <strong>$15.00</strong>
-                  </td>
-                  <td className="col-sm-1 col-md-1 text-center">
-                    <strong>$150.00</strong>
-                  </td>
-                  <td className="col-sm-1 col-md-1 text-center">
-                    <label><input type="checkbox"></input></label>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>   </td>
-                  <td>   </td>
-                  <td>   </td>
-                  <td>
-                    <h5>Subtotal</h5>
-                  </td>
-                  <td className="text-right">
-                    <h5>
-                      <strong>$24.59</strong>
-                    </h5>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>   </td>
-                  <td>   </td>
-                  <td>   </td>
-                  <td>
-                    <h5>Estimated shipping</h5>
-                  </td>
-                  <td className="text-right">
-                    <h5>
-                      <strong>$6.94</strong>
-                    </h5>
-                  </td>
-                </tr>
-                <tr>
-                  <td>   </td>
-                  <td>   </td>
-                  <td>   </td>
-                  <td>
-                    <h3>Total</h3>
-                  </td>
-                  <td className="text-right">
-                    <h3>
-                      <strong>$31.53</strong>
-                    </h3>
-                  </td>
-                </tr>
-                <tr>
-                  <td>   </td>
-                  <td>   </td>
-                  <td>   </td>
-                  <td>
-                    <button type="button" className="btn btn-default">
-                      <span className="glyphicon glyphicon-shopping-cart" />{" "}
-                      Continue Shopping
-                    </button>
-                  </td>
-                  <td>
-                    <button type="button" className="btn btn-success">
-                      Checkout <span className="glyphicon glyphicon-play" />
-                    </button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+                  </div>
+                  <div className="col-sm-10">
+                    <h4 className="nomargin">Product 1</h4>
+                    <p>awfag</p>
+                  </div>
+                </div>
+              </td>
+              <td data-th="Price">$1.99</td>
+              <td data-th="Quantity">
+                <input type="number" className="form-control text-center" />
+              </td>
+              <td data-th="Subtotal" className="text-center">
+                1.99
+              </td>
+              <td className="actions" data-th="">
+                <button className="btn btn-info btn-sm">
+                  <i className="fa fa-refresh" />
+                </button>
+                <button className="btn btn-danger btn-sm">
+                  <i className="fa fa-trash-o" />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr className="visible-xs">
+              <td className="text-center">
+                <strong>Total 1.99</strong>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <a href="#" style={{width: "fit-content"}} className="btn btn-warning">
+                  <i style={{width: "fit-content"}}/> Clear All
+                </a>
+              </td>
+              <td colSpan="2" className="hidden-xs" />
+              <td className="hidden-xs text-center">
+                <strong>Total $1.99</strong>
+              </td>
+              <td>
+                <a href="#" className="btn btn-success btn-block">
+                  Continue Shopping <i className="fa fa-angle-right" />
+                </a>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     );
   }
