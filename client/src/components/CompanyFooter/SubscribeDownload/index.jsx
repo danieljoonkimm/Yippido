c692813f6250087b5c60e4ed680016fc2f5e396c
Daniel Kim <danieljoonkimm@gmail.com>
2019-05-04 15:35:15 -0700

    [edit] fixing font sizes

--- client/src/components/CompanyFooter/SubscribeDownload/index.jsx
+++ client/src/components/CompanyFooter/SubscribeDownload/index.jsx
@@ -10,26 +10,26 @@ class FooterSubscribeDownload extends Component {
     return (
       <div id="footerSubscribeDownload_container">
         <div className="row" id="footerSubscribeDownload_row">
-          <p>
+          <p className="footerSubscribeDownload_p">
             Sign up for the latest style tips, news and exclusive promotional
             offers.
           </p>
           <div className="col-xs-12 col-sm-12 col-md-12">
-            <button>Subscribe</button>
+            <button id="footerSubscribeDownload_button">Subscribe</button>
           </div>
 
           <div className="col-xs-12 col-sm-12 col-md-12">
-            <p>Download our app</p>
+            <p className="footerSubscribeDownload_p">Download our app</p>
           </div>
 
           <div className="col-xs-12 col-sm-12 col-md-12">
             <div className="row">
-              <div className="col-xs-6 col-sm-6 col-md-6">
-                <button>APP</button>
+              <div className="col-xs-6 col-sm-6 col-md-6" id="footerSubscribeDownload_buttonApp">
+                <button className="footerSubscribeDownload_button">APP</button>
               </div>
 
-              <div className="col-xs-6 col-sm-6 col-md-6">
-                <button>Google Play</button>
+              <div className="col-xs-6 col-sm-6 col-md-6" id="footerSubscribeDownload_buttonGoogle">
+                <button className="footerSubscribeDownload_button">Google Play</button>
               </div>
             </div>
           </div>
