dc3e06331022b6eddf7f8ad0966c01cf32d79c53
Daniel Kim <danieljoonkimm@gmail.com>
2019-05-04 17:58:21 -0700

    [edit] fixing company header

--- client/src/components/CompanyHeader/index.jsx
+++ client/src/components/CompanyHeader/index.jsx
@@ -1,25 +1,23 @@
 import React, { Component } from "react";
 import Yippido from "../../../public/images/Yippido.png";
 import "./companyHeader.scss";
-import { CountryDropdown } from 'react-country-region-selector';
+import { CountryDropdown } from "react-country-region-selector";
 
 class CompanyHeader extends Component {
   constructor() {
     super();
 
-    this.state = { country: ''};
+    this.state = { country: "" };
   }
 
   inputSelect() {
     let input = document.querySelectorAll("input");
     for (var i = 0; i < input.length; i++) {
-      input[i].setAttribute(
-        input[i].getAttribute("placeholder").length
-      );
+      input[i].setAttribute(input[i].getAttribute("placeholder").length);
     }
   }
 
-  selectCountry (val) {
+  selectCountry(val) {
     this.setState({ country: val });
   }
 
@@ -27,56 +25,73 @@ class CompanyHeader extends Component {
     const { country } = this.state;
     return (
       <div className="container" id="companyHeader_container">
-        <div className="row" id="companyHeader_row">
-          <div className="col-xs-3 col-sm-3 col-md-3 companyHeader_img">
-            <img src={Yippido} alt="Yippido" style={{ width: "10em" }} />
+        <div className="col-xs-3 col-sm-3 col-md-3">
+          <img src={Yippido} />
+        </div>
+
+        <div className="col-xs-6 col-sm-6 col-md-6">
+          <div className="row" id="companyHeader_row">
+            <div className="input-group">
+              <div className="input-group-btn search-panel">
+                <button
+                  type="button"
+                  class="btn btn-default dropdown-toggle"
+                  data-toggle="dropdown"
+                >
+                  <span id="search_concept">Filter by</span>{" "}
+                  <span class="caret" />
+                </button>
+                <ul>
+                  <li>
+                    <a href="#contains">Contains</a>
+                  </li>
+                  <li>
+                    <a href="#its_equal">It's equal</a>
+                  </li>
+                  <li>
+                    <a href="#greather_than">Greather than ></a>
+                  </li>
+                  <li>
+                    <a href="#less_than">Less than</a>
+                  </li>
+                  <li class="divider" />
+                  <li>
+                    <a href="#all">Anything</a>
+                  </li>
+                </ul>
+              </div>
+              <input
+                type="hidden"
+                name="search_param"
+                value="all"
+                id="search_param"
+              />
+              <input
+                type="text"
+                class="form-control"
+                name="x"
+                placeholder="type vendor name, item description, or style number"
+              />
+              <span class="input-group-btn">
+                <button class="btn btn-default" type="button">
+                  <span class="glyphicon glyphicon-search" />
+                </button>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-xs-3 col-sm-3 col-md-3">
+          <div className="row">
+            <div className="col-xs-6 col-sm-6 col-md-6">Select Country</div>
+
+            <div className="col-xs-6 col-sm-6 col-md-6">Help</div>
           </div>
 
-          <div className="col-xs-6 col-sm-6 col-md-6 companyHeader_searchBar">
-            <div className="row companyHeader_searchbar">
-            <div className="col-xs-2 col-sm-2 col-md-2 companyHeader_searchAll">
-              <button id="companyHeader_searchButton">All</button>
-            </div>
-              <div className="col-xs-9 col-sm-9 col-md-9 companyHeader_searchInput">
-                <input
-                  className="companyHeader_search_input"
-                  type="text"
-                  name=""
-                  placeholder="type vendor name, item description, or style number"
-                />
-              </div>
-              <div className="col-xs-1 col-sm-1 col-md-1 companyHeader_searchIcon">
-                <a href="#" className="companyHeader_search_icon">
-                  <i className="fas companyHeader_fa-search" />
-                </a>
-              </div>
-            </div>
-          </div>
+          <div className="row">
+            <div className="col-xs-6 col-sm-6 col-md-6">Register</div>
 
-          <div className="col-xs-3 col-sm-3 col-md-3 companyHeader_selectCompanyRegister">
-            <div className="row">
-              <div className="col-xs-6 col-sm-6 col-md-6">
-              <CountryDropdown
-              placeholder="Select Country"
-              className="col-xs-6 country"
-              value={country}
-              onChange={(val) => this.selectCountry(val)}></CountryDropdown>
-              </div>
-
-              <div className="col-xs-6 col-sm-6 col-md-6">
-                <button>Help</button>
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col-xs-6 col-sm-6 col-md-6">
-                <button>Register</button>
-              </div>
-
-              <div className="col-xs-6 col-sm-6 col-md-6">
-                <button>SIGN IN</button>
-              </div>
-            </div>
+            <div className="col-xs-6 col-sm-6 col-md-6">Sign In</div>
           </div>
         </div>
       </div>
