63b9483ff805a9ecb1df899c921ec3f589e0f88c
blake-swl <seulwoolee93@gmail.com>
2019-05-08 01:18:19 -0700

    [edit] navigation

--- client/src/components/Navigation/NavCategory/navSubCategory.jsx
+++ client/src/components/Navigation/NavCategory/navSubCategory.jsx
@@ -9,29 +9,13 @@ export default class NavSubCategory extends Component {
   renderSubCategories() {
     let category = this.props.subItems;
     return category.map(subCategory => {
-      return subCategory.categories.map(child => {
-        return (
-          <ul className="subCategory_name">{subCategory.name}
-            <li className="subCategory_item">
-              {child.name}
-            </li>
-          </ul>
-        )
-      })
+      return (<ul><li className={"subCategory_name " + (subCategory.categories.length > 0  ? 'group' : 'single') } key={subCategory.category_id}>{subCategory.name}
+        {subCategory.categories.map(child => {
+          return <li key={child.category_id} className="subCategory_item">{child.name}</li>
+        })}
+        </li></ul>)
     })
   }
-  // renderSubCategories() {
-  //   let category = this.props.subItems;
-  //   return category.map(subCategory => {
-  //     console.log(subCategory)
-  //     return (<ul><li className="subCategory_name" key={subCategory.category_id}>{subCategory.name}
-  //       {subCategory.categories.map(child => {
-  //         console.log(child.name)
-  //         return <li key={child.category_id} className="subCategory_item">{child.name}</li>
-  //       })}
-  //       </li></ul>)
-  //   })
-  // }
 
   render() {
     let category = this.props.subItems;
