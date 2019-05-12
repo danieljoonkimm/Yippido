ae957f4be2b9c0b7a57c60102d7b3391f642b871
Daniel Kim <danieljoonkimm@gmail.com>
2019-05-11 21:04:51 -0700

    [edit] fixing shopping cart

--- client/src/components/ShoppingCart/VendorShoppingCartSavedItems/index.jsx
+++ client/src/components/ShoppingCart/VendorShoppingCartSavedItems/index.jsx
@@ -10,8 +10,7 @@ class VendorShoppingCartSavedItems extends Component {
         products: [
           {
             key: "726",
-            thumb:
-              "http://www.davidhechler.com/wp-content/uploads/2016/07/500x500-dummy-image.jpg",
+            thumb: "https://img.pranavc.in/200",
             name: "iPod Shuffle",
             points: 0,
             product_id: "34",
@@ -24,13 +23,14 @@ class VendorShoppingCartSavedItems extends Component {
             recurring: "",
             total: "$244.00",
             price_raw: 122,
-            total_raw: 244
+            total_raw: 244,
+            color: "red",
+            size: ["S", "M", "L"]
           },
           {
             key: "726",
-            thumb:
-              "http://www.davidhechler.com/wp-content/uploads/2016/07/500x500-dummy-image.jpg",
-            name: "iPod Shuffle",
+            thumb: "https://img.pranavc.in/200",
+            name: "iPod Shuffle2",
             points: 0,
             product_id: "34",
             model: "Product 7",
@@ -42,7 +42,9 @@ class VendorShoppingCartSavedItems extends Component {
             recurring: "",
             total: "$244.00",
             price_raw: 122,
-            total_raw: 244
+            total_raw: 244,
+            color: "blue",
+            size: ["S", "M", "L"]
           }
         ]
       }
@@ -63,24 +65,85 @@ class VendorShoppingCartSavedItems extends Component {
     this.state = {};
   }
 
-  convertShoppingCartHeader(data) {
-    return data.map(data => {
-      return <div>
-        <table>
-          <thead>
-            <tr>{data}</tr>
-          </thead>
-        </table>
-      </div>
+  convertShoppingCartHeader(shoppingCartHeader, shoppingCartItems) {
+    let shoppingCartHeaderShow = shoppingCartHeader.map(data => {
+      return <tr>{data}</tr>;
     });
+    console.log(shoppingCartHeaderShow);
+
+    let lengthOfProducts = shoppingCartItems.length;
+
+    let shoppingCartItemsShow = shoppingCartItems.map(data => {
+      return (
+        <tr>
+          <td>
+            <img src={data.thumb} style={{ width: "50px", height: "auto" }} />
+          </td>
+          <td>{data.key}</td>
+          <td>{data.color}</td>
+          <td>{data.size}</td>
+          <td style={{display: "flex"}}>
+            <button
+              type="button"
+              class="btn btn-default btn-number"
+              disabled="disabled"
+              data-type="minus"
+              data-field="quant[1]"
+            >
+              <span class="glyphicon glyphicon-minus" />
+            </button>
+            <input
+              type="text"
+              name="quant[1]"
+              class="form-control input-number"
+              value="1"
+              min="1"
+            />
+            <button
+              type="button"
+              class="btn btn-default btn-number"
+              data-type="plus"
+              data-field="quant[1]"
+            >
+              <span class="glyphicon glyphicon-plus" />
+            </button>
+          </td>
+          <td>{lengthOfProducts}</td>
+          <td>{data.price}</td>
+          <td>{data.total}</td>
+          <td><input type="checkbox"/></td>
+        </tr>
+      );
+    });
+
+    // <div class="input-group">
+    //       <span class="input-group-btn">
+    //           <button type="button" class="btn btn-default btn-number" disabled="disabled" data-type="minus" data-field="quant[1]">
+    //               <span class="glyphicon glyphicon-minus"></span>
+    //           </button>
+    //       </span>
+    //       <input type="text" name="quant[1]" class="form-control input-number" value="1" min="1" max="10">
+    //       <span class="input-group-btn">
+    //           <button type="button" class="btn btn-default btn-number" data-type="plus" data-field="quant[1]">
+    //               <span class="glyphicon glyphicon-plus"></span>
+    //           </button>
+    //       </span>
+    //   </div>
+    return (
+      <table className="table">
+        <thead style={{ display: "flex" }}>{shoppingCartHeaderShow}</thead>
+        <tbody>{shoppingCartItemsShow}</tbody>
+      </table>
+    );
   }
 
   render() {
     return (
       <div className="container" id="vendorShoppingCartSavedItems_container">
-        <div className="row" style={{display: "flex"}}>
-          {this.convertShoppingCartHeader(this.shoppingCartHeader)}
-        </div>
+        {this.convertShoppingCartHeader(
+          this.shoppingCartHeader,
+          this.data.CompanyName.products
+        )}
       </div>
     );
   }
