022d94290512919fdaf34c95c0353c5b5de7c15c
Daniel Kim <danieljoonkimm@gmail.com>
2019-05-12 23:25:22 -0700

    [edit] fixing shipping

--- client/src/components/ShippingPayment/index.jsx
+++ client/src/components/ShippingPayment/index.jsx
@@ -43,33 +43,64 @@ class ShippingPayment extends Component {
   convertShippingInformation(data) {
     return data.map(info => {
       return (
-        <div className="col-xs-6 col-sm-6 col-md-6 shippingPaymentInfo" style={{padding: "10px"}}>
-        <div className="row" style={{border: "1px solid black", padding: "10px", lineHeight: "20px"}}>
-          <div className="col-xs-12 col-sm-12 col-md-12" style={{paddingBottom: "10px"}}>{info.name}</div>
-          <div className="col-xs-12 col-sm-12 col-md-12">
-            {info.address} {info.unit}
+        <div
+          className="col-xs-12 col-sm-6 col-md-6 shippingPaymentInfo"
+          style={{ padding: "10px" }}
+        >
+          <div
+            className="row"
+            style={{
+              border: "1px solid black",
+              padding: "10px",
+              lineHeight: "20px",
+              fontSize: "12px"
+            }}
+          >
+            <div
+              className="col-xs-12 col-sm-12 col-md-12"
+              style={{ paddingBottom: "10px" }}
+            >
+              {info.name}
+            </div>
+            <div className="col-xs-12 col-sm-12 col-md-12">
+              {info.address} {info.unit}
+            </div>
+            <div className="col-xs-12 col-sm-12 col-md-12">
+              {info.city} {info.state} {info.zip}
+            </div>
+            <div
+              className="col-xs-12 col-sm-12 col-md-12"
+              style={{ paddingBottom: "10px" }}
+            >
+              {info.country}
+            </div>
+            <div className="col-xs-12 col-sm-12 col-md-12">
+              <div className="col-xs-6 col-sm-6 col-md-6">{info.phone}</div>
+              <div className="col-xs-3 col-sm-3 col-md-3">
+    
+                  <span class="glyphicon glyphicon-pencil">Edit</span>
+             
+              </div>
+            </div>
           </div>
-          <div className="col-xs-12 col-sm-12 col-md-12">
-            {info.city} {info.state} {info.zip}
-          </div>
-          <div className="col-xs-12 col-sm-12 col-md-12" style={{paddingBottom: "10px"}}>{info.country}</div>
-          <div className="col-xs-12 col-sm-12 col-md-12">{info.phone}</div>
-        </div>
         </div>
       );
     });
   }
 
-  addNewAddress(address) {
-
-  }
+  addNewAddress(address) {}
 
   render() {
     return (
       <div className="container">
         <div className="row">
-          <h2>Shipping Information</h2>
-          <div className="col-xs-8 col-sm-8 col-md-8">
+          <div
+            className="col-xs-12 col-sm-8 col-md-8"
+            style={{ border: "1px solid black", padding: "10px" }}
+          >
+            <h2 style={{ paddingLeft: "10px" }}>
+              <strong>Shipping Information</strong>
+            </h2>
             {this.convertShippingInformation(this.shippingInformation)}
           </div>
         </div>
