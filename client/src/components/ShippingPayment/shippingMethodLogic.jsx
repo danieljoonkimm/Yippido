a20586fa03ed3aebe2310d07c07a248216df1698
Daniel Kim <danieljoonkimm@gmail.com>
2019-05-13 01:14:58 -0700

    [edit] needing to fix shipping methods logic

--- client/src/components/ShippingPayment/shippingMethodLogic.jsx
+++ client/src/components/ShippingPayment/shippingMethodLogic.jsx
@@ -1,29 +1,51 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class ShippingMethodLogic extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.shipping = [
+      {
+        UPS: [
+          "UPS Ground 2-3 DAYS",
+          "UPS 2nd Day",
+          "UPS Next Day",
+          "UPS 3rd Day"
+        ]
+      },
+      {
+        Trucking: ["Truck"]
+      },
+      {
+        Other: ["Labeling Service"]
+      }
+    ];
+
+    this.state = {};
   }
 
   shippingMethodLogic(logic) {
-    return(
-      <div className="col-xs-12 col-sm-12">
+    return (
+      <div className="col-xs-12 col-sm-12 col-md-12">
         <div className="col-xs-4 col-sm-4 col-md-4">
-          <img src ={logic.companyLogo}/>
+          <img src={logic.companyLogo} />
         </div>
       </div>
-    )
+    );
+  }
+
+  shippingMethodsAvailable(shipping) {
+
   }
 
   render() {
     return (
       <div className="row">
         {this.shippingMethodLogic(this.props.companyInformation)}
+        {this.shippingMethodsAvailable(this.shipping)}
       </div>
-    )
+    );
   }
-};
+}
 
 export default ShippingMethodLogic;
