bdeaa2d770377bf43e9435d10c8237cd2d2cd5a6
Daniel Kim <danieljoonkimm@gmail.com>
2019-05-12 23:56:00 -0700

    [edit] fixing

--- client/src/components/ShippingPayment/index.jsx
+++ client/src/components/ShippingPayment/index.jsx
@@ -5,6 +5,19 @@ class ShippingPayment extends Component {
   constructor() {
     super();
 
+    this.orderSummary = [
+      {
+        orderCount: 5,
+        shippingHandling: 15.0,
+        orderTotal: 93.0
+      },
+      {
+        orderCount: 4,
+        shippingHandling: 15.0,
+        orderTotal: 165.0
+      }
+    ];
+
     this.shippingInformation = [
       {
         name: "Hans Kim",
@@ -77,9 +90,7 @@ class ShippingPayment extends Component {
             <div className="col-xs-12 col-sm-12 col-md-12">
               <div className="col-xs-6 col-sm-6 col-md-6">{info.phone}</div>
               <div className="col-xs-3 col-sm-3 col-md-3">
-    
-                  <span class="glyphicon glyphicon-pencil">Edit</span>
-             
+                <span class="glyphicon glyphicon-pencil">Edit</span>
               </div>
             </div>
           </div>
@@ -88,7 +99,30 @@ class ShippingPayment extends Component {
     });
   }
 
-  addNewAddress(address) {}
+  convertOrderSummary(order) {
+    let orderLength = order.length;
+    let totalAmount = 0;
+    let orderTotal = order.map(total => {
+      totalAmount = total.orderTotal + totalAmount;
+    });
+
+    return (
+      <div
+        className="col-xs-12 col-sm-12 col-md-12"
+        style={{ padding: "10px" }}
+      >
+        <div className="col-xs-12 col-sm-12 col-md-12">{orderLength}</div>
+        <div className="col-xs-6 col-sm-6 col-md-6">Order Total:</div>
+        <div className="col-xs-6 col-sm-6 col-md-6">{totalAmount}</div>
+        <div className="col-xs-12 col-sm-12 col-md-12">
+          <button>Place Order</button>
+        </div>
+        <div className="col-xs-12 col-sm-12 col-md-12">
+          <p>Every order you place with us is safe & secure</p>
+        </div>
+      </div>
+    );
+  }
 
   render() {
     return (
@@ -96,12 +130,31 @@ class ShippingPayment extends Component {
         <div className="row">
           <div
             className="col-xs-12 col-sm-8 col-md-8"
-            style={{ border: "1px solid black", padding: "10px" }}
+            style={{ padding: "10px" }}
           >
-            <h2 style={{ paddingLeft: "10px" }}>
-              <strong>Shipping Information</strong>
-            </h2>
-            {this.convertShippingInformation(this.shippingInformation)}
+            <div
+              className="row"
+              style={{ border: "1px solid black", padding: "10px" }}
+            >
+              <h2 style={{ paddingLeft: "10px" }}>
+                <strong>Shipping Information</strong>
+              </h2>
+              {this.convertShippingInformation(this.shippingInformation)}
+            </div>
+          </div>
+
+          <div
+            className="col-xs-12 col-sm-4 col-md-4"
+            style={{
+              padding: "10px",
+              lineHeight: "20px",
+              fontSize: "12px"
+            }}
+          >
+          <div className="row" style={{ border: "1px solid black", padding: "10px" }}>
+            <h2 style={{ paddingLeft: "10px" }}>Order Summary</h2>
+            {this.convertOrderSummary(this.orderSummary)}
+          </div>
           </div>
         </div>
       </div>
