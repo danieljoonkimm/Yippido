10036e9b4491b5c6a9eb63464fcf8aceb6051ddc
Daniel Kim <danieljoonkimm@gmail.com>
2019-05-16 01:00:32 -0700

    [edit] fixing shipping protect package section

--- client/src/components/ShippingPayment/shippingProtectPackage.jsx
+++ client/src/components/ShippingPayment/shippingProtectPackage.jsx
@@ -4,13 +4,51 @@ class ShippingProtectPackage extends Component {
   constructor() {
     super();
 
-    this.state = {}
+    this.options = [
+      "Yes, please protect my package(s) with insurance",
+      "Yes, please protect my package(s) with signature requirement *Additional fees will be applied"
+    ]
+
+
+    this.state = {
+      isChecked: false,
+      checkedData: []
+    }
+  }
+
+  handleCheckbox(e) {
+    console.log(e.target.name, this.state.isChecked)
+    if(this.state.isChecked === false) {
+      this.setState({
+        isChecked: !this.state.isChecked
+      })
+    } else {
+      this.setState({
+        isChecked: !this.state.isChecked
+      })
+    }
+  }
+
+  convertPackageOptions(options) {
+    return(
+      options.map(options => {
+        return (
+          <div className="form-group">
+            <input type="checkbox" name="eachSelector" onClick={this.handleCheckbox.bind(this)}/> {options}
+          </div>
+        )
+      })
+    )
   }
 
   render() {
+    const protectPackage = "Would you like to protect your package(s)?"
     return(
       <div className="row">
-        hi
+        <h2>{protectPackage}</h2>
+        <div className="col-xs-9 col-sm-9 col-md-9">
+          {this.convertPackageOptions(this.options)}
+        </div>
       </div>
     )
   }
