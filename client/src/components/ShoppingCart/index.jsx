8b178a74a5e94572bff8926bb5e9ddf1ad1f79af
Daniel Kim <danieljoonkimm@gmail.com>
2019-05-28 17:19:08 -0700

    [edit] fixing shopping cart

--- client/src/components/ShoppingCart/index.jsx
+++ client/src/components/ShoppingCart/index.jsx
@@ -35,7 +35,14 @@ class ShoppingCart extends Component {
 
     return reactSteps.map( steps => {
      return Object.keys(steps).map(step => {
-       return <div className="col-xs-4 col-sm-4 col-md-4">{step}</div>
+       return <div className="col-xs-4 col-sm-4 col-md-4">
+                <div className="col-xs-10 col-sm-10 col-md-10">
+                  <img style={{height: "20px", width: "auto"}} src="https://image.flaticon.com/icons/svg/2/2772.svg"/>
+                </div>
+                <div className="col-xs-10 col-sm-10 col-md-10">
+                  {step}
+                </div>
+              </div>
      })
     })
   }
@@ -53,7 +60,7 @@ class ShoppingCart extends Component {
         id="shoppingCart_container"
         style={{ fontSize: "11px" }}
       >
-        <div className="row"><div className="col-xs-9 col-sm-9 col-md-9">{this.constructShoppingCartProgressBar()}</div></div>
+        <div className="row"><div className="col-xs-9 col-sm-9 col-md-9" style={{paddingLeft: "0px"}}>{this.constructShoppingCartProgressBar()}</div></div>
         
         <Tabs className="row" selectedTabClassName="settings__activeTab">
           <div className="col-xs-3 col-sm-3 col-md-3">
